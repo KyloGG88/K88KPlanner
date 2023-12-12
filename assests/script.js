66b7905ab127c302823b127acf2671aed35f1b54
Kyle GG <goliath.kylec@gmail.com>
2023-12-12 22:26:48 +0000

    Pseudo 1

--- assests/script.js
+++ assests/script.js
@@ -4,3 +4,8 @@ const today = dayjs().format('dddd, MMMM DD');
 
 dayView.append(today);
 
+// Make timeblocks for each hour
+        // div consisting of textarea and save btn
+        // can either hardcode in html or generate with js/jquery
+        // Important: each time block has a unique id or data attribute corresponding to the hour number it is for
+        // ideally that attribute can correspond to the key you use to store the text in local storage
