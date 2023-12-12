67ef25722e8c1bf6e438f81c1282afb5d37a2325
Kyle GG <goliath.kylec@gmail.com>
2023-12-12 22:38:23 +0000

    update 1.0

--- assests/script.js
+++ assests/script.js
@@ -8,4 +8,25 @@ dayView.append(today);
         // div consisting of textarea and save btn
         // can either hardcode in html or generate with js/jquery
         // Important: each time block has a unique id or data attribute corresponding to the hour number it is for
-        // ideally that attribute can correspond to the key you use to store the text in local storage
+// ideally that attribute can correspond to the key you use to store the text in local storage
+        
+$(function () {
+  for (let i = 9; i < 18; i++) {
+    // Variables for planner area
+    let mainContainer = $(".container");
+    let hoursRow = $("<div>");
+    let hour = $("<p>");
+    let textContainer = $("<textarea>");
+    let saveButton = $("<button>");
+    let saveIcon = $("<i>");
+    
+    // Adding classes to elements
+    hoursRow.addClass("time-block row");
+    hour.addClass("hour col-1");
+    textContainer.addClass("textarea col-10");
+    saveIcon.addClass("fa-solid fa-floppy-disk");
+    saveButton.addClass("saveBtn col-1");
+
+    mainContainer.append(hoursRow);
+  };
+};
